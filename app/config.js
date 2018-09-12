4e8d3b8bca47c6d2c6fd81f6e2feaf8807c4b247
Aaron Robinson <aaron.robinson@education.gov.uk>
2018-09-12 13:36:19 +0100

    Push Davids updates

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Manage your education and skills funding',
+  serviceName: 'Education and skills funding service',
   // Default port that prototype runs on
   port: '3000',
 
