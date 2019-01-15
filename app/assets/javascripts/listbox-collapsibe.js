b12a6b8be5a3bfbfef5e69793be9104476468d4f
Chris Harding <chris@transparentux.co.uk>
2019-01-15 09:24:10 +0000

    New design for a filter that sits above the document list.

---
+++ app/assets/javascripts/listbox-collapsibe.js
@@ -0,0 +1,72 @@
+/**
+ * ARIA Collapsible Dropdown Listbox Example
+ * @function onload
+ * @desc Initialize the listbox example once the page has loaded
+ */
+
+
+var aria = aria || {};
+
+aria.ListboxButton = function (button, listbox,click) {
+  this.button = button;
+  this.listbox = listbox;
+  this.registerEvents();
+  this.function = click;
+};
+
+aria.ListboxButton.prototype.registerEvents = function () {
+  this.button.addEventListener('click', this.showListbox.bind(this));
+  this.button.addEventListener('keyup', this.checkShow.bind(this));
+  this.listbox.listboxNode.addEventListener('blur', this.hideListbox.bind(this));
+  this.listbox.listboxNode.addEventListener('keydown', this.checkHide.bind(this));
+  this.listbox.setHandleFocusChange(this.onFocusChange.bind(this));
+
+  this.listbox.listboxNode.addEventListener('click',this.clicked.bind(this))
+};
+
+aria.ListboxButton.prototype.clicked = function (evt) {
+  this.function(evt.target.getAttribute("data-filter"))
+
+};
+
+aria.ListboxButton.prototype.checkShow = function (evt) {
+  var key = evt.which || evt.keyCode;
+
+  switch (key) {
+    case aria.KeyCode.UP:
+    case aria.KeyCode.DOWN:
+      evt.preventDefault();
+      this.showListbox();
+      this.listbox.checkKeyPress(evt);
+      break;
+  }
+};
+
+aria.ListboxButton.prototype.checkHide = function (evt) {
+  var key = evt.which || evt.keyCode;
+
+  switch (key) {
+    case aria.KeyCode.RETURN:
+    case aria.KeyCode.ESC:
+      evt.preventDefault();
+      this.hideListbox();
+      this.button.focus();
+      break;
+  }
+};
+
+aria.ListboxButton.prototype.showListbox = function () {
+  console.log("clicked")
+  aria.Utils.removeClass(this.listbox.listboxNode, 'hidden');
+  this.button.setAttribute('aria-expanded', 'true');
+  this.listbox.listboxNode.focus();
+};
+
+aria.ListboxButton.prototype.hideListbox = function () {
+  aria.Utils.addClass(this.listbox.listboxNode, 'hidden');
+  this.button.removeAttribute('aria-expanded');
+};
+
+aria.ListboxButton.prototype.onFocusChange = function (focusedItem) {
+  this.button.innerText = focusedItem.innerText;
+};
