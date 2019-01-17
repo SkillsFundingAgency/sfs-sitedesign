742da2f1648e58e23e9d6690cf0fbf2cdc9eb346
Chris Harding <chris@transparentux.co.uk>
2019-01-17 14:02:27 +0000

    Added jQuery logic so content is now dynamic on the select academy/school page.

---
+++ app/assets/javascripts/beta/v15-0-0/search-radio-buttons.js
@@ -0,0 +1,24 @@
+
+
+/**********
+ * SEARCH RADIO BUTTONS (JavaScript)
+ * **********/
+
+// jQuery (START)
+$(document).ready(function () {
+
+    // $('input[name="academyOrSchoolName"]').click(function() {
+    $('input[name="academyOrSchoolName"]').on('change', function() {
+        
+        var selectedRadioButtonValue = $('input[name=academyOrSchoolName]:checked').val(); 
+
+        $("#clearSearchAndSelection").text("Clear search and selection");
+        $("#selectedChoiceHeading").text("You’ve selected “" + selectedRadioButtonValue + "”");
+    });
+
+});
+// jQuery (END)
+
+// JavaScript (START)
+
+// JavaScript (END)
