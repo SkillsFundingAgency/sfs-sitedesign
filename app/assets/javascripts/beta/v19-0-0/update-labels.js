7fd3cff116a74339f8a7e39c99b1cea5937150c3
Chris Harding <chris@transparentux.co.uk>
2019-03-11 17:38:37 +0000

    Added new revised user journey for LA training providers within the Document Exchange (External) child view.

--- app/assets/javascripts/beta/v19-0-0/update-labels.js
+++ app/assets/javascripts/beta/v19-0-0/update-labels.js
@@ -14,67 +14,197 @@ $(document).ready(function () {
 
     // NEW document received #1
     $(".document1").click(function() {
-        window.location = "received-from-esfa?paginationRequired=true&page1=true&documentReceived1=Downloaded";
+
+        var currentURL = window.location.href;
+        var doesThisContainQ = currentURL.includes("q");
+
+        if (doesThisContainQ == true) {
+            window.location = window.location.href + "&documentReceived1=Downloaded";
+        }
+        else {
+            window.location = window.location.href + "?documentReceived1=Downloaded";
+        }
+        
     });
 
     // NEW document received #2
     $(".document2").click(function() {
-        window.location = "received-from-esfa?paginationRequired=true&page1=true&documentReceived2=Downloaded";
+
+        var currentURL = window.location.href;
+        var doesThisContainQ = currentURL.includes("q");
+
+        if (doesThisContainQ == true) {
+            window.location = window.location.href + "&documentReceived2=Downloaded";
+        }
+        else {
+            window.location = window.location.href + "?documentReceived2=Downloaded";
+        }
+
     });
 
     // NEW document received #3
     $(".document3").click(function() {
-        window.location = "received-from-esfa?paginationRequired=true&page1=true&documentReceived3=Downloaded";
+
+        var currentURL = window.location.href;
+        var doesThisContainQ = currentURL.includes("q");
+
+        if (doesThisContainQ == true) {
+            window.location = window.location.href + "&documentReceived3=Downloaded";
+        }
+        else {
+            window.location = window.location.href + "?documentReceived3=Downloaded";
+        }
+
     });
 
     // NEW document received #4
     $(".document4").click(function() {
-        window.location = "received-from-esfa?paginationRequired=true&page1=true&documentReceived4=Downloaded";
+
+        var currentURL = window.location.href;
+        var doesThisContainQ = currentURL.includes("q");
+
+        if (doesThisContainQ == true) {
+            window.location = window.location.href + "&documentReceived4=Downloaded";
+        }
+        else {
+            window.location = window.location.href + "?documentReceived4=Downloaded";
+        }
+
     });
 
     // NEW document received #5
     $(".document5").click(function() {
-        window.location = "received-from-esfa?paginationRequired=true&page1=true&documentReceived5=Downloaded";
+
+        var currentURL = window.location.href;
+        var doesThisContainQ = currentURL.includes("q");
+
+        if (doesThisContainQ == true) {
+            window.location = window.location.href + "&documentReceived5=Downloaded";
+        }
+        else {
+            window.location = window.location.href + "?documentReceived5=Downloaded";
+        }
+
     });
 
     // NEW document received #6
     $(".document6").click(function() {
-        window.location = "received-from-esfa?paginationRequired=true&page1=true&documentReceived6=Downloaded";
+
+        var currentURL = window.location.href;
+        var doesThisContainQ = currentURL.includes("q");
+
+        if (doesThisContainQ == true) {
+            window.location = window.location.href + "&documentReceived6=Downloaded";
+        }
+        else {
+            window.location = window.location.href + "?documentReceived6=Downloaded";
+        }
+
     });
 
     // NEW document received #7
     $(".document7").click(function() {
-        window.location = "received-from-esfa?paginationRequired=true&page1=true&documentReceived7=Downloaded";
+
+        var currentURL = window.location.href;
+        var doesThisContainQ = currentURL.includes("q");
+
+        if (doesThisContainQ == true) {
+            window.location = window.location.href + "&documentReceived7=Downloaded";
+        }
+        else {
+            window.location = window.location.href + "?documentReceived7=Downloaded";
+        }
+
     });
 
     // NEW document received #8
     $(".document8").click(function() {
-        window.location = "received-from-esfa?paginationRequired=true&page1=true&documentReceived8=Downloaded";
+
+        var currentURL = window.location.href;
+        var doesThisContainQ = currentURL.includes("q");
+
+        if (doesThisContainQ == true) {
+            window.location = window.location.href + "&documentReceived8=Downloaded";
+        }
+        else {
+            window.location = window.location.href + "?documentReceived8=Downloaded";
+        }
+
     });
 
     // NEW document received #9
     $(".document9").click(function() {
-        window.location = "received-from-esfa?paginationRequired=true&page1=true&documentReceived9=Downloaded";
+
+        var currentURL = window.location.href;
+        var doesThisContainQ = currentURL.includes("q");
+
+        if (doesThisContainQ == true) {
+            window.location = window.location.href + "&documentReceived9=Downloaded";
+        }
+        else {
+            window.location = window.location.href + "?documentReceived9=Downloaded";
+        }
+
     });
 
     // NEW document received #10
     $(".document10").click(function() {
-        window.location = "received-from-esfa?paginationRequired=true&page1=true&documentReceived10=Downloaded";
+
+        var currentURL = window.location.href;
+        var doesThisContainQ = currentURL.includes("q");
+
+        if (doesThisContainQ == true) {
+            window.location = window.location.href + "&documentReceived10=Downloaded";
+        }
+        else {
+            window.location = window.location.href + "?documentReceived10=Downloaded";
+        }
+
     });
 
     // NEW document received #11
     $(".document11").click(function() {
-        window.location = "received-from-esfa?paginationRequired=true&page1=true&documentReceived11=Downloaded";
+
+        var currentURL = window.location.href;
+        var doesThisContainQ = currentURL.includes("q");
+
+        if (doesThisContainQ == true) {
+            window.location = window.location.href + "&documentReceived11=Downloaded";
+        }
+        else {
+            window.location = window.location.href + "?documentReceived11=Downloaded";
+        }
+
     });
 
     // NEW document received #12
     $(".document12").click(function() {
-        window.location = "received-from-esfa?paginationRequired=true&page1=true&documentReceived12=Downloaded";
+
+        var currentURL = window.location.href;
+        var doesThisContainQ = currentURL.includes("q");
+
+        if (doesThisContainQ == true) {
+            window.location = window.location.href + "&documentReceived12=Downloaded";
+        }
+        else {
+            window.location = window.location.href + "?documentReceived12=Downloaded";
+        }
+
     });
 
     // NEW document received #13
     $(".document13").click(function() {
-        window.location = "received-from-esfa?paginationRequired=true&page1=true&documentReceived13=Downloaded";
+
+        var currentURL = window.location.href;
+        var doesThisContainQ = currentURL.includes("q");
+
+        if (doesThisContainQ == true) {
+            window.location = window.location.href + "&documentReceived13=Downloaded";
+        }
+        else {
+            window.location = window.location.href + "?documentReceived13=Downloaded";
+        }
+
     });
 
     /***
