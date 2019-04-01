7daef6f31920c79114176370baef180046654f39
Chris Harding <chris@transparentux.co.uk>
2019-04-01 12:54:03 +0100

    Additions to the PE and sport user journeys and a revised design (tweak) to the PDF for allocation statements (adults).

--- app/routes/beta/v20-0-0/pe-and-sport.js
+++ app/routes/beta/v20-0-0/pe-and-sport.js
@@ -30,7 +30,58 @@ module.exports = function(router) {
 	// Search or download
 	router.get('/' + version + '/not-signed-in/pe-and-sport/2018-to-2019/search-or-download', function (req, res) {
 		res.render(version + '/not-signed-in/pe-and-sport/2018-to-2019/search-or-download', {
-			'version' : version
+			'version' : version,
+			'error' : req.query.error
+		});
+	});
+	router.post('/' + version + '/not-signed-in/pe-and-sport/2018-to-2019/search-or-download', function (req, res) {
+		
+		var searchOn = req.body.searchOrDownload;
+		var nameOrLAENumber = req.body.nameOrLAENumber;
+		var nameOrCode = req.body.nameOrCode;
+		
+		if (searchOn == "School or academy name or local authority establishment number") {
+			
+			req.session.searchTerm = nameOrLAENumber;
+
+			// Added so we can see an error page (search term which returns zero results)
+			if (nameOrLAENumber == "Foxford School" || nameOrLAENumber == "foxford school" || nameOrLAENumber == "Mole Catch Academy" || nameOrLAENumber == "mole catch academy") {
+				res.redirect('/' + version + '/not-signed-in/pe-and-sport/2018-to-2019/no-results');
+			}
+			else {
+				res.redirect('/' + version + '/not-signed-in/pe-and-sport/2018-to-2019/name-or-lae-number');
+			}
+			
+		}
+		else if (searchOn == "Local authority name or code") {
+
+			req.session.searchTerm = nameOrCode;
+
+			// Added so we can see an error page (search term which returns zero results)
+			if (nameOrCode == "Foxford School" || nameOrCode == "foxford school" || nameOrCode == "Mole Catch Academy" || nameOrCode == "mole catch academy") {
+				res.redirect('/' + version + '/not-signed-in/pe-and-sport/2018-to-2019/no-results');
+			}
+			else {
+				res.redirect('/' + version + '/not-signed-in/pe-and-sport/2018-to-2019/name-or-code');
+			}			
+			
+		}
+		// Make sure the user chooses an option
+		else {
+			res.redirect('/' + version + '/not-signed-in/pe-and-sport/2018-to-2019/search-or-download?error=true');
+		}
+		
+	});
+
+	/**********
+	 * ERROR PAGES
+	 * **********/
+
+	// Display when users enter a search term which returns zero results
+	router.get('/' + version + '/not-signed-in/pe-and-sport/2018-to-2019/no-results', function (req, res) {
+		res.render(version + '/not-signed-in/pe-and-sport/2018-to-2019/no-results', {
+			'version' : version,
+			'searchTerm' : req.session.searchTerm
 		});
 	});
 
