4372aa872d7756f89db9a2150269d36799fa34cd
Chris Harding <chris@transparentux.co.uk>
2019-07-31 08:34:32 +0100

    Made all guidance design changes to the view your funding (DSG and PE and sports) user journeys.

--- app/assets/javascripts/beta/v101-0-0/filters-and-checkboxes.js
+++ app/assets/javascripts/beta/v101-0-0/filters-and-checkboxes.js
@@ -7,6 +7,17 @@
 // jQuery (START)
 $(document).ready(function () {
 
+    // Ensure we close the filters on load of the page only if the user is viewing in a smaller resolution
+    /*
+    if ((screen.width >= 1024) && (screen.height >= 1024)) {
+        
+        if () {
+
+        }
+
+    }
+    */
+
     // Trigger the select all documents in document list rules (see below) via clicking "Select/deselect all team members" link
     $("#selectAllTrigger").click(function () {
         $("#document-checkbox").trigger("click");
