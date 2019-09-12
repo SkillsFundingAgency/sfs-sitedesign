82b2e12c3fd81308c299eca087f61389133e036c
Chris Harding <chris@transparentux.co.uk>
2019-09-12 12:13:49 +0100

    Some of the final design changes to the VYF prototype (v104.0.0) for this beta phase.

--- app/assets/javascripts/beta/v104-0-0/custom-global-behaviour.js
+++ app/assets/javascripts/beta/v104-0-0/custom-global-behaviour.js
@@ -7,6 +7,42 @@
 // jQuery (START)
 $(document).ready(function () {
 
+    // Onload of the page set the DEFAULT CSS to be applied to the print view of the UI served to the user (this is updated dynamically on click below)
+    var hashURL = window.location.hash;
+
+    if (hashURL == "#schools") {
+        $('head').append('<style type="text/css" media="print">#schools { display: inherit; } #css { display: none; } #high-needs { display: none; } #early-years { display: none; }</style>');
+    }
+    else if (hashURL == "#css") {
+        $('head').append('<style type="text/css" media="print">#schools { display: none; } #css { display: inherit; } #high-needs { display: none; } #early-years { display: none; }</style>');
+    }
+    else if (hashURL == "#high-needs") {
+        $('head').append('<style type="text/css" media="print">#schools { display: none; } #css { display: none; } #high-needs { display: inherit; } #early-years { display: none; }</style>');
+    }
+    else if (hashURL == "#early-years") {
+        $('head').append('<style type="text/css" media="print">#schools { display: none; } #css { display: none; } #high-needs { display: none; } #early-years { display: inherit; }</style>');
+    }
+
+    // Dynamically update the CSS being applied to the print view of the UI served to the user
+    $(".print-trigger").click(function () {
+
+        var clickedPrintSection = $(this).attr('href');
+        
+        if (clickedPrintSection == "#schools") {
+            $('head').append('<style type="text/css" media="print">#schools { display: inherit; } #css { display: none; } #high-needs { display: none; } #early-years { display: none; }</style>');
+        }
+        else if (clickedPrintSection == "#css") {
+            $('head').append('<style type="text/css" media="print">#schools { display: none; } #css { display: inherit; } #high-needs { display: none; } #early-years { display: none; }</style>');
+        }
+        else if (clickedPrintSection == "#high-needs") {
+            $('head').append('<style type="text/css" media="print">#schools { display: none; } #css { display: none; } #high-needs { display: inherit; } #early-years { display: none; }</style>');
+        }
+        else if (clickedPrintSection == "#early-years") {
+            $('head').append('<style type="text/css" media="print">#schools { display: none; } #css { display: none; } #high-needs { display: none; } #early-years { display: inherit; }</style>');
+        }
+
+    });
+
     // Trigger the select all documents in document list rules (see below) via clicking "Select/deselect all team members" link
     $(".toggle-wrap").click(function (e) {
         
@@ -33,6 +69,39 @@ $(document).ready(function () {
             $('.gem-c-related-navigation__toggle-more').hide();
             $('.gem-c-related-navigation__toggle-more').addClass("js-hidden");
             $('.gem-c-related-navigation__toggle-more').attr("aria-hidden", true);
+        }
+
+    });
+
+    // Global function to deal with expanding and closing all hidden links in the service, currently covering:
+    // a) Download component
+    // b) Support component
+    $(".expand-hidden-section").click(function (e) {
+        
+        e.preventDefault();
+
+        var expandHiddenSectionState = $(this).attr('aria-expanded');
+        var hiddenSection = $(this).attr('href');
+
+        if (expandHiddenSectionState == "false") {
+            // Update the parent (trigger) link
+            $(this).attr("data-expanded", true);
+            $(this).attr("aria-expanded", true);
+            // Then update the child hidden section           
+            $(hiddenSection).show();
+            $(hiddenSection).removeClass("js-hidden");
+            $(hiddenSection).attr("aria-hidden", false);
+            $(hiddenSection).css("display", "inline-block");
+        }
+        else if (expandHiddenSectionState == "true") {
+            // Update the parent (trigger) link
+            $(this).attr("data-expanded", false);
+            $(this).attr("aria-expanded", false);
+            // Then update the child hidden section           
+            $(hiddenSection).hide();
+            $(hiddenSection).addClass("js-hidden");
+            $(hiddenSection).attr("aria-hidden", true);
+            $(hiddenSection).css("display", "none");
         }
 
     });
