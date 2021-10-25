5ae5e278ab5e378583f2f89dea65734dab874f66
xkyuuji <hi@kyuuji.com>
2021-10-25 14:53:05 +0100

    Recoupment Prototype
    Added recoupment report page with several tables
    Linked to guidance for each table
    Added consistent elements with existing statements
    Added variance selection for recoupment

--- app/routes/beta/v123-0-0/signed-in/allocation-statements.js
+++ app/routes/beta/v123-0-0/signed-in/allocation-statements.js
@@ -909,6 +909,38 @@ router.get('/' + version + '/signed-in/sign-in-roles-alt/view-your-roles', funct
 	});
 });
 
+/* - - - - - - - - - - - - - - - - - -- - - - - - - - - - - - - - - - - - - - - - - */
+/* - - - - ALTERNATE VERSION OF SIGN-IN-ROLES BASED ON PRE-PROD ENVIRONMENT - - - - */
+/* - - - - - - - - - - - - - - - - - -- - - - - - - - - - - - - - - - - - - - - - - */
+
+router.get('/' + version + '/signed-in/recoupment/variance-selection', function (req, res) {
+
+	res.render(version + '/signed-in/recoupment/variance-selection', {
+		'version' : version,
+		'versioning' : req.session.versioning,
+		'myRolesAndPermissionsURL' : req.session.myRolesAndPermissionsURL,
+		'signOutURL' : req.session.signOutURL,
+		'dashboard' : req.session.dashboard,
+		'idams' : req.session.idams,
+		'organisationName' : req.session.organisationName,
+		'scenario' : req.query.scenario
+	});
+});
+
+router.get('/' + version + '/signed-in/recoupment/recoupment-report', function (req, res) {
+
+	res.render(version + '/signed-in/recoupment/recoupment-report', {
+		'version' : version,
+		'versioning' : req.session.versioning,
+		'myRolesAndPermissionsURL' : req.session.myRolesAndPermissionsURL,
+		'signOutURL' : req.session.signOutURL,
+		'dashboard' : req.session.dashboard,
+		'idams' : req.session.idams,
+		'organisationName' : req.session.organisationName,
+		'scenario' : req.query.scenario
+	});
+});
+
 
 
 	/**********
